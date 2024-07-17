44268a03b028fce84771eba5b3fd6c62399b8768
Max Keller <max.keller2312@gmail.com>
2024-07-17 19:56:19 +0200

    First change

---
+++ max_test_folder/test_train_mnist_fashion.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data='fashion-mnist', epochs=100, imgsz=28)
